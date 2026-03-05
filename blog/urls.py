e2fa36e09ff383b87a7776ac632c023d469b707e
Douglas Carneiro <douglas.ul@gmail.com>
2026-03-05 17:03:13 -0300

    Prática 02: views e urls

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('welcome/', views.welcome, name='welcome'),
+    path('eco/<str:text>/', views.eco, name='eco'),
+    path('info/', views.info, name='info'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Bem-vindo ao meu blog!')
+
+def eco(request, text):
+    return HttpResponse(f'Você digitou: {text}')
+
+def info(request):
+    info = {
+        "disciplina": "RAD",
+        "framework": "Django",
+        "semestre": "2025.2"
+    }
+    return JsonResponse(info)
