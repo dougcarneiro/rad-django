bb07d38d982f2f140927f6f784fc517871b92df1
Douglas Carneiro <douglas.ul@gmail.com>
2026-03-18 17:20:08 -0300

    Prática 03: templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('welcome/', views.welcome, name='welcome'),
     path('eco/<str:text>/', views.eco, name='eco'),
     path('info/', views.info, name='info'),
+    path('home/', views.home, name='home'),
+    path('contato/<str:telefone>/', views.contato, name='contato'),
+    path('loops/', views.loops, name='loops'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
 
 # Create your views here.
 def welcome(request):
@@ -15,3 +16,34 @@ def info(request):
         "semestre": "2025.2"
     }
     return JsonResponse(info)
+
+def home(request):
+    is_logged_in = request.user.is_authenticated
+    role = 'admin' if is_logged_in and request.user.is_staff else 'user'
+
+    contexto = {
+        "usuario": "Douglas",
+        "data_atual": timezone.now(),
+        "is_logged_in": is_logged_in,
+        "role": role,
+    }
+    return render(request, 'blog/home.html', contexto)
+
+
+def contato(request, telefone):
+    contexto = {
+        "telefone": telefone,
+    }
+    return render(request, 'blog/contato.html', contexto)
+
+
+def loops(request):
+    produtos = [
+        {"nome": "Notebook", "preco": 3500.00},
+        {"nome": "Mouse", "preco": 120.50},
+        {"nome": "Teclado", "preco": 210.90},
+    ]
+    contexto = {
+        "produtos": produtos,
+    }
+    return render(request, 'blog/loops.html', contexto)
