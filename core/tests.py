58f2031ab69e91f70eeee5f069df582ceb4e81c5
Douglas Carneiro <douglas.ul@gmail.com>
2026-05-28 16:58:29 -0300

    Prática 10: Testes em Django

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class URLTestCase(SimpleTestCase):
+    def test_home_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        
+    
--- edu/tests.py
+++ edu/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+from edu.models import Autor
+
+class AutorTestCase(TestCase):
+    def setUp(self):
+        Autor.objects.create(nome="Gabriel García Márquez",)
+        
+    def test_autor_existe(self):
+        autor = Autor.objects.get(nome="Gabriel García Márquez")
+        self.assertEqual(autor.nome, "Gabriel García Márquez")
+    
+    def test_autor_nao_existe(self):
+        with self.assertRaises(Autor.DoesNotExist):
+            Autor.objects.get(nome="Jorge Luis Borges")
+
+class AutorSerializerTestCase(TestCase):
+    def test_autor_serializer(self):
+        from edu.serializers import AutorSerializer
+        form_data = {'nome': 'Isabel Allende'}
+        serializer = AutorSerializer(data=form_data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data['nome'], "Isabel Allende")
+
+class AutorFormTestCase(TestCase):
+    def test_autor_form(self):
+        from edu.forms import AutorForm
+        form_data = {'nome': 'Mario Vargas Llosa'}
+        form = AutorForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+    def test_autor_form_invalido(self):
+        from edu.forms import AutorForm
+        form_data = {'nome': ''}
+        form = AutorForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('nome', form.errors)
+        
+class APIAutorTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.autor = Autor.objects.create(nome="Chinua Achebe")
+    
+    def test_api_autor_list(self):
+        from rest_framework.test import APIClient
+        client = APIClient()
+        response = client.get('/edu/api/autor/')
+        self.assertEqual(response.status_code, 200)
